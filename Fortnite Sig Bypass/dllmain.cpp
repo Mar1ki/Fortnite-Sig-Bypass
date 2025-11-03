5bce6845903610bb4aea9680a8ca37b57083e2b8
Gav <1khosting8@gmail.com>
2025-11-02 16:19:46 -0800

    Update dllmain.cpp

--- Fortnite Sig Bypass/dllmain.cpp
+++ Fortnite Sig Bypass/dllmain.cpp
@@ -6,8 +6,8 @@ DWORD WINAPI Main(LPVOID)
 {
     static auto Base = __int64(GetModuleHandleW(0));
     WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x4B421F8), "\x90\x90\x90\x90\x90\x90", 6, NULL); // pak is invalid
-    WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x3610198), "\xC7\x03\x00\x00\x00\x00", 6, NULL); // failed to decrypt toc signature
-    WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x36079AA), "\x41\xC7\x04\x24\x00\x00\x00\x00", 8, NULL); // missing signature
+    WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x3610198 + 2), "\x00", 1, NULL); // failed to decrypt toc signature
+    WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x36079AA + 4), "\x00", 1, NULL); // missing signature
 
     return 0;
 }
