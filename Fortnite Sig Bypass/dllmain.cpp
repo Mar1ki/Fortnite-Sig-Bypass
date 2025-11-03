f2af89785bac93358f3e85e4bd9eac37eddd3a35
Gav <1khosting8@gmail.com>
2025-11-02 16:43:19 -0800

    Update dllmain.cpp

--- Fortnite Sig Bypass/dllmain.cpp
+++ Fortnite Sig Bypass/dllmain.cpp
@@ -5,13 +5,16 @@ using namespace std;
 DWORD WINAPI Main(LPVOID)
 {
     static auto Base = __int64(GetModuleHandleW(0));
+    BYTE invalidpakBytes[6];
+    ReadProcessMemory(GetCurrentProcess(), (LPCVOID)(Base + 0x4D75F88), invalidpakBytes, 6, NULL);
+
     WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x4D75F88), "\x90\x90\x90\x90\x90\x90", 6, NULL); // pak is invalid
     WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x3838C78 + 2), "\x00", 1, NULL); // failed to decrypt toc
     WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x383048A + 4), "\x00", 1, NULL); // missing signature
     Sleep(3500); // Restore bytes before loading screen or else it crashes
-    WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x4D75F88), "\x0F\x84\x39\x09\x00\x00", 6, NULL); // pak is invalid
-    WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x3838C78 + 2), "\x00", 1, NULL); // failed to decrypt toc
-    WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x383048A + 4), "\x0C", 8, NULL); // missing signature
+    WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x4D75F88), invalidpakBytes, 6, NULL); // pak is invalid
+    WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x3838C78 + 2), "\x0C", 1, NULL); // failed to decrypt toc
+    WriteProcessMemory(GetCurrentProcess(), (LPVOID)(Base + 0x383048A + 4), "\x0C", 1, NULL); // missing signature
 
     return 0;
 }
